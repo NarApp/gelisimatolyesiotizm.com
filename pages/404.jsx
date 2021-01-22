008fa24bc26bc825444b8e73210298834d0ecf69
Mehmet KARACA <mehmet.karaca@narapp.com>
2021-01-23 00:13:01 +0300

    -

--- pages/404.jsx
+++ pages/404.jsx
@@ -1,5 +1,5 @@
 import Head from 'next/head'
 export default ()=>
 <Head>
-<meta http-equiv="refresh" content="2;url=https://gelisimatolyesiotizm.com" />
+<meta http-equiv="refresh" content="0;url=https://gelisimatolyesiotizm.com" />
 </Head>
